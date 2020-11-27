75a2066cc8463883a4ab257abd19557f2224b144
bcl31 <17791229+bcl31@users.noreply.github.com>
2020-11-26 18:32:14 -0700

    grid creation functionality

--- grid.py
+++ grid.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+
+def create_grid(filename):
+    grid = []
+    grid_info = open(filename, "r")
+    row_number = int(grid_info.readline())
+    row_width = int(grid_info.readline())
+    for row_location in range(row_number):
+        row = []
+        for item_pos in range(row_width):
+            row.append(grid_info.readline())
+        grid.append(row)
+    return grid
+
+
+print(create_grid("data_1.txt"))
